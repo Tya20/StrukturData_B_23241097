2712af07f9b3bbfad6468f430c7df9b849913bbf
Tya20 <146092759+Tya20@users.noreply.github.com>
2024-05-06 12:03:29 +0800

    kuliah 06-05-2024

---
+++ Stuktur data/meet6.py
@@ -0,0 +1,71 @@
+# perbandingan 
+# lebih besar >
+# lebih kecil <
+# lebih besar sama dengan  >=
+# lebih kecil sama dengan <=
+# sama dengan == 
+# tidak sama dengan !=
+# sama "is" 
+# tidak sama "is not"
+
+x = 4
+y = 5
+
+# lebih besar >
+hasil = x > y
+print (x, '>', y, 'adalah', hasil)
+
+# lebih kecil >
+hasil = x < y
+print (x, '<', y, 'adalah', hasil)
+
+# lebih besar sama dengan >==
+hasil = x >= 2
+print ( x, '>=',2, 'adalah', hasil)
+hasil = x >= 4
+print ( x, '>=',4, 'adalah', hasil)
+
+# lebih kecil sama dengan <=
+hasil = x <= 2
+print ( x, '<=',2, 'adalah', hasil)
+hasil = x <= 4
+print ( x, '<=',4, 'adalah', hasil)
+
+# sama dengan ==
+hasil = x == 7
+print (x, '==', 7, 'adalah', hasil)
+
+# tidak sama dengan !
+hasil = x !=7
+print (x, '!', 7, 'adalah', hasil)
+
+# sama "is"
+hasil = x is 7
+print (x, 'is', 7, 'adalah', hasil)
+
+# tidak sama "is not"
+hasil = x is not 7
+print (x, 'is not', 7, 'adalah', hasil)
+
+# > < >= <= == != ini adalah perbandingan literal
+# x = 4, 4 = literal (tidak memakan memory)
+# x = objeck
+#
+# x = 4 (bisa) 
+# x is 4 (tidak bisa karena yang dibandingkan adl literal 
+# x = is y (bisa , karena yd dibandingkan adalah object) 
+
+# is dan isnot
+hasil = x is 4
+print (x, ' is ' ,y, 'adalah ', hasil)
+
+hasil = x is  not 4
+print (x, ' is not ' ,4, 'adalah ', hasil)
+
+
+
+
+
+
+
+
