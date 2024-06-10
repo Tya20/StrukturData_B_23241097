f197dccc93f4ddd44d0ff84ad5cc5a3c5e50dfa9
Tya20 <146092759+Tya20@users.noreply.github.com>
2024-06-10 12:25:38 +0800

    10/juni/2024

---
+++ meet10.py/meet10.py
@@ -0,0 +1,32 @@
+# belajar perbandingan lanjutan, elif 
+
+nama = input (" masukan nama : "  )
+
+if nama=="mustiati": # kondisi 1
+    print ("mahasiswa")# aksi true 2
+elif nama=="mustiati": # kondisi 2
+    print("mahasiswa undikma") # aksi true 2
+elif nama=="Tya": # kondisi 3
+    print (" mahasiswa") # aksi true 3
+else:
+    print ("saya mahasiswa") # aksi flase
+    print ("program seleasi")
+
+
+# applikasi penjumlahan sederhana
+
+#memimta input dari pengguna
+num1=input("masukkan angka pertama:")
+num2=input ("masukkan angka kedua: ")
+
+#mengubah input menjadi bilangan bulan
+num1=int(num1)
+num2=int(num2)
+
+#Menjumlahkan dua bilangan 
+result= num1+num2
+#Menampilkan hasil pengumlahan 
+print("Hasil penjumlahan:", result)
+
+
+
