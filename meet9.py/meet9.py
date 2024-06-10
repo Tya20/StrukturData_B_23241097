b59d01979d8cab8266230a02288e50c7ad62c6b7
Tya20 <146092759+Tya20@users.noreply.github.com>
2024-06-10 11:52:05 +0800

    3/juni/2024

---
+++ meet9.py/meet9.py
@@ -0,0 +1,21 @@
+# pecabanga jika maka 
+name = input ("what is your name :")
+
+# kondisi : aksi
+#if standar
+#if name=="mustiati":print("mahasiswa pti")
+#print ("end of program")
+
+#if name=="mustiati"
+#   print ("mahasiswa pti")
+#    print("dari fstt")
+#print ("end of program")
+
+#. if else
+if name=="mustiati":
+    print("mahasiswa pti")
+    print (f"Terimaksi{name}")
+else:
+    print("bukan mahasiswa pti")
+    print ("end of program")
+
